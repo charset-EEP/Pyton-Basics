7cb561d40c3a203cd62cc25390203f9863b48d44
EricCetre <ericepaulo@hotmail.com>
2019-08-08 09:51:27 -0300

    exercicios 1/1-3

---
+++ 1/py3.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+x=int(input(print("Digite o primeiro número")))
+y=int(input(print("Digite o segundo número")))
+x=x+y
+print("Resultado da soma :"+str(x))
