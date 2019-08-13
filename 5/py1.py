f0c4d12c0d9228cd5d66eef5f2fe5d7ff39dbc99
EricCetre <ericepaulo@hotmail.com>
2019-08-13 11:11:48 -0300

    Criação de todos arquivos

--- 5/py1.py
+++ 5/py1.py
@@ -1,7 +1,1 @@
-#Faça um programa para imprimir:
-
-        #1
-        #2   2
-        #3   3   3
-        #.....
-        #n   n   n   n   n   n  ... n
+#
--- 8/py1.py
+++ 8/py1.py
@@ -1,4 +1,1 @@
-#Classe Bola: Crie uma classe que modele uma bola:
-
-    #Atributos: Cor, circunferência, material
-    #Métodos: trocaCor e mostraCor 
+#
